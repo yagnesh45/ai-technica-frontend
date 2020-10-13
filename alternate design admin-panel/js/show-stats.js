7fae3887c14ee17eed9c98173ba9308d9eda4980
Yagnesh45 <yagneshpatil43@gmail.com>
2020-10-14 00:01:29 +0530

    added show-stats.js to business and profit

--- alternate design admin-panel/js/show-stats.js
+++ alternate design admin-panel/js/show-stats.js
@@ -1,31 +1,31 @@
-var i, moneyContent;
+var i = 0, moneyContent;
 
-        moneyContent = document.getElementsByClassName("money-daily");
-        for (i = 0; i < moneyContent.length; i++) {
-            moneyContent[i].style.display = "block";
-        }
-        function showMoney(evt, moneyID){
-            var i, moneyContent, tripsContent, tabLinks, activeMoney;
+moneyContent = document.getElementsByClassName("money-daily");
+for (i = 0; i < moneyContent.length; i++) {
+    moneyContent[i].style.display = "block";
+}
+function showMoney(evt, moneyID) {
+    var i, moneyContent, tripsContent, tabLinks, activeMoney;
 
-            moneyContent = document.getElementsByClassName("money-div");
-            for (i = 0; i < moneyContent.length; i++) {
-                moneyContent[i].style.display = "none";
-            }
+    moneyContent = document.getElementsByClassName("money-div");
+    for (i = 0; i < moneyContent.length; i++) {
+        moneyContent[i].style.display = "none";
+    }
 
-            tripsContent = document.getElementsByClassName("trips-digits");
-            for (i = 0; i < moneyContent.length; i++) {
-                tripsContent[i].style.display = "none";
-            }
+    tripsContent = document.getElementsByClassName("trips-digits");
+    for (i = 0; i < moneyContent.length; i++) {
+        tripsContent[i].style.display = "none";
+    }
 
-            tablinks = document.getElementsByClassName("calendar-link");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", " inactive");
-            }
+    tablinks = document.getElementsByClassName("calendar-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", " inactive");
+    }
 
-            activeMoney = document.getElementsByClassName(moneyID);
-            for (i = 0; i < tablinks.length; i++) {
-                activeMoney[i].style.display="block";
-            }
+    activeMoney = document.getElementsByClassName(moneyID);
+    for (i = 0; i < activeMoney.length; i++) {
+        activeMoney[i].style.display = "block";
+    }
 
-            evt.currentTarget.className += " active";
-        }
+    evt.currentTarget.className += " active";
+}
